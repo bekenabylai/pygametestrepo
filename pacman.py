68f148c4fe72d11c62035495845c5291edf4fbd8
NurDAULET <nurik.02311@gmail.com>
2021-12-23 00:05:22 +0600

    images qosyld

--- pacman.py
+++ pacman.py
@@ -31,3 +31,24 @@ acceleration = 0.5
 # main surface
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Hungry Pacman")
+
+# images
+background = pygame.image.load('SPACE.jpg')
+
+score_backround = pygame.image.load('score_background.png').convert_alpha()
+score_background_rect = score_backround.get_rect()
+score_background_rect.topleft = (10,10)
+
+live_background = pygame.image.load('score_background.png').convert_alpha()
+live_background_rect = live_background.get_rect()
+live_background_rect.topleft = (WINDOW_WIDTH-(live_background_rect.width+10), 10)
+
+packman_image = pygame.image.load('PACKMAN.png').convert_alpha()
+packman_image = pygame.transform.scale(packman_image, (50, 80))  #pACKMAN umenshit
+packman_rect = packman_image.get_rect()
+packman_rect.center = (80, WINDOW_HEIGHT//2)
+
+ghostpck_image = pygame.image.load('ghostpck.png').convert_alpha()
+ghostpck_image = pygame.transform.scale(ghostpck_image, (40, 60)) #Ghost umenshit
+ghostpck_image_rect = ghostpck_image.get_rect()
+ghostpck_image_rect.center = ((WINDOW_WIDTH+BUFFER_DISTANCE), random.randint(HEADER_HEIGHT+25, WINDOW_HEIGHT-25))
