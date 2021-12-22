76faf55f434af539a7460b568f01cbfaa288007b
Abylai <aby.beken@aues.kz>
2021-12-23 00:18:15 +0600

    sound and music jane main loop kosyldi oin tolyk

--- pacman.py
+++ pacman.py
@@ -87,3 +87,116 @@ game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2)
 game_continue_text = main_font.render("PRESS ANY KEY", True, GREEN)
 game_continue_rect = game_continue_text.get_rect()
 game_continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2+100)
+
+# sounds and musics
+pygame.mixer.music.load('music.wav')
+sound1 = pygame.mixer.Sound('sound_1.wav')
+sound2 = pygame.mixer.Sound('sound_2.wav')
+sound2.set_volume(0.1)
+
+FPS = 60
+clock = pygame.time.Clock()
+
+pygame.mixer.music.play(-1, 0.0)
+# main loop
+fl_pause = False
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and packman_rect.top > HEADER_HEIGHT:
+        packman_rect.y -= ghostpck_velocity
+    elif keys[pygame.K_DOWN] and packman_rect.bottom < WINDOW_HEIGHT:
+        packman_rect.y += ghostpck_velocity
+
+    ghostpck_image_rect.centerx -= ghostpck_velocity
+    blueghostpacman_image_rect.centerx -= blueghostpacman_velocity
+    yellowghostpacman_image_rect.centerx -= yellowghostpacman_velocity
+
+
+    if ghostpck_image_rect.centerx < 0:
+        ghostpck_image_rect.center = ((WINDOW_WIDTH+BUFFER_DISTANCE), random.randint(HEADER_HEIGHT+25, WINDOW_HEIGHT-25))
+        player_live -= 1
+        sound2.play()
+    if blueghostpacman_image_rect.centerx < 0:
+        blueghostpacman_image_rect.center = ((WINDOW_WIDTH+BUFFER_DISTANCE), random.randint(HEADER_HEIGHT+25, WINDOW_HEIGHT-25))
+        player_live -= 1
+        sound2.play()
+    if yellowghostpacman_image_rect.centerx < 0:
+        yellowghostpacman_image_rect.center = ((WINDOW_WIDTH+BUFFER_DISTANCE), random.randint(HEADER_HEIGHT+25, WINDOW_HEIGHT-25))
+        player_live -= 1
+        sound2.play()
+
+
+
+
+    if packman_rect.colliderect(ghostpck_image_rect):
+        player_score += 1
+        sound1.play()
+        ghostpck_image_rect.center = ((WINDOW_WIDTH+BUFFER_DISTANCE), random.randint(HEADER_HEIGHT+25, WINDOW_HEIGHT-25))
+        ghostpck_velocity += acceleration
+
+    if packman_rect.colliderect(blueghostpacman_image_rect):
+        player_score += 1
+        player_live +=2
+        sound1.play()
+        blueghostpacman_image_rect.center = ((WINDOW_WIDTH-BUFFER_DISTANCE), random.randint(HEADER_HEIGHT+25, WINDOW_HEIGHT-25))
+        blueghostpacman_velocity += acceleration
+    
+    if packman_rect.colliderect(yellowghostpacman_image_rect):
+        player_score -= 1
+        player_live -=1
+        sound1.play()
+        yellowghostpacman_image_rect.center = ((WINDOW_WIDTH-BUFFER_DISTANCE), random.randint(HEADER_HEIGHT+25, WINDOW_HEIGHT-25))
+        yellowghostpacman_velocity += acceleration
+
+        
+    if player_live == 0 or player_score >15:
+        fl_pause = True
+        pygame.mixer.music.stop()
+        live_text = main_font.render("LIFE: " + str(player_live), True, RED)
+        display_surface.blit(live_text, live_text_rect)
+        pygame.display.update()
+        while fl_pause:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    fl_pause = False
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    player_score = PLAYER_STARTING_SCORE
+                    player_live = PLAYER_STARTING_LIVE
+                    ghostpck_velocity = GHOSTPCK_STARTING_VELOCITY
+                    blueghostpacman_velocity = GHOSTPCK_STARTING_VELOCITY
+                    yellowghostpacman_velocity = GHOSTPCK_STARTING_VELOCITY
+                    pygame.mixer.music.play(-1, 0.0) 
+                    fl_pause = False
+
+            display_surface.blit(game_over_text, game_over_rect)
+            display_surface.blit(game_continue_text, game_continue_rect)
+            pygame.display.update()
+
+
+    display_surface.blit(background, (0,0))
+    display_surface.blit(score_backround, score_background_rect)
+    display_surface.blit(live_background, live_background_rect)
+    pygame.draw.line(display_surface, WHITE, (0, HEADER_HEIGHT), (WINDOW_WIDTH, HEADER_HEIGHT), 5)
+    display_surface.blit(packman_image, packman_rect)
+    display_surface.blit(ghostpck_image, ghostpck_image_rect)
+    display_surface.blit(blueghostpacman_image, blueghostpacman_image_rect)
+    display_surface.blit(yellowghostpacman_image, yellowghostpacman_image_rect)
+    
+    score_text = main_font.render("POINT: " + str(player_score), True, RED)
+    live_text = main_font.render("LIFE: " + str(player_live), True, RED)
+    display_surface.blit(score_text, score_text_rect)
+    display_surface.blit(game_name, game_name_rect)
+    display_surface.blit(live_text, live_text_rect)
+    
+
+
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
